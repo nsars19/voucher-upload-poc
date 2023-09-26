776ace541186cbffabae6dc0e23c72a7d5c83f05
Nick Sardonini <nsardonini@umassp.edu>
2023-09-26 16:51:05 -0400

    create better variable names for base64 encoding functions

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,15 +1,17 @@
 import { useCallback } from "react";
 
 const storage = window?.localStorage;
+const base64Encode = btoa;
+const base64Decode = atob;
 
 export const useLocalStorage = () => {
   const get = useCallback((item) => {
-    return JSON.parse(atob(storage.getItem(item)));
+    return JSON.parse(base64Decode(storage.getItem(item)));
   }, []);
 
   const set = useCallback((key, item) => {
     try {
-      storage.setItem(key, btoa(JSON.stringify(item)));
+      storage.setItem(key, base64Encode(JSON.stringify(item)));
     } catch (e) {
       const msg = "unable to stringify item " + item;
       console.log(msg);
